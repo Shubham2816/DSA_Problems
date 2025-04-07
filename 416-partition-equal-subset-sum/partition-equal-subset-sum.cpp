8fcc0610eeec114293fa8a660caa66ff83dca77a
Shubham Kumar <96763863+Shubham2816@users.noreply.github.com>
2025-04-07 11:55:46 +0530

    Time: 130 ms (48.05%) | Memory: 100.4 MB (38.60%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -11,7 +11,7 @@ public:
 
         if(dp[ind][sum] != -1) return dp[ind][sum];
 
-        bool notTake = func(ind - 1, sum, nums, dp);
+        bool notTake = func(ind - 1, sum, nums, dp); //func(2, 11)
         bool take = false;
         if(nums[ind] <= sum){
             take = func(ind - 1, sum - nums[ind], nums, dp);
@@ -25,16 +25,17 @@ public:
         int sum = 0;
         for(int i : nums){
             sum += i;
+            // int sum = 1 + 5 + 5 + 11 = 22
         }
 
         if(sum % 2 == 1){
             return false;
         }
 
-        sum = sum / 2;
+        sum = sum / 2; //11
 
         vector<vector<int>> dp(n, vector<int>(sum+1, -1));
         return func(n-1, sum, nums, dp);
-
+// func(3, 11).
     }
 };
