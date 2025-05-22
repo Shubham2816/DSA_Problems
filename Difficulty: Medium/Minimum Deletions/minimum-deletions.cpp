cb02940cf251f1baedb21baac8baa9332a3a02fb
Shubham Kumar <96763863+Shubham2816@users.noreply.github.com>
2025-05-22 12:36:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Deletions/minimum-deletions.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+  
+   int LPS(string S, int n){
+       
+        string T=S;
+        reverse(S.begin(),S.end());
+        vector<int> curr(n+1,0), prev(n+1,0);
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(S[i-1]==T[j-1]) curr[j]=1+prev[j-1];
+                else curr[j]=max(prev[j],curr[j-1]);
+            }
+            prev=curr;
+        }
+        return curr[n];
+   }
+  
+    int minDeletions(string s) {
+        // code here
+          int n=s.length();
+        int lps_len=LPS(s,n);
+        return n-lps_len;
+    }
+};
