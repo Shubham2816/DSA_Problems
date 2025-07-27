79c7c39a31f412e9df708506b27344865e4df830
Shubham Kumar <96763863+Shubham2816@users.noreply.github.com>
2025-07-27 12:09:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.cpp
@@ -0,0 +1,29 @@
+// User function Template for C++
+class Solution {
+  public:
+    void setMatrixZeroes(vector<vector<int>> &mat) {
+        // code here
+          int n = mat.size();
+        int m = mat[0].size();
+        
+        vector<bool> rows(n, false);
+        vector<bool> cols(m, false);
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rows[i] or cols[j])
+                    mat[i][j] = 0;
+            }
+        }
+    
+    }
+};
