0cc9ecb0e16eb355fcc542b72dad8db7a715b6f6
Shubham Kumar <96763863+Shubham2816@users.noreply.github.com>
2025-07-31 11:27:14 +0530

    Time: 1002 ms (65.07%) | Memory: 339.9 MB (29.97%) - LeetSync

---
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& arr) {
+        unordered_set<int> result_ors;
+        unordered_set<int> current_ors;
+
+         for (int x : arr) {
+            // `next_ors` will store the ORs of subarrays ending at the current element `x`.
+            std::unordered_set<int> next_ors;
+            
+            // The subarray of just the element x gives an OR of x.
+            next_ors.insert(x);
+
+            // Compute new ORs by extending previous subarrays with the current element x.
+            for (int y : current_ors) {
+                next_ors.insert(x | y);
+            }
+
+            // Add all newly found ORs to the main result set.
+            result_ors.insert(next_ors.begin(), next_ors.end());
+            
+            // For the next iteration, the current results become the previous results.
+            current_ors = next_ors;
+        }
+
+        return result_ors.size();
+    }
+};
