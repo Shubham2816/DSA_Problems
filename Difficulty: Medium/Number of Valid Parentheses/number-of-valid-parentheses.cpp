faa367528f8ebe5acb546a371db1ff4756ac83f7
Shubham Kumar <96763863+Shubham2816@users.noreply.github.com>
2026-01-25 10:21:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Valid Parentheses/number-of-valid-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+ public:
+    virtual int fun(int n, int sum){
+      if(sum<0) return 0;
+      if(n==0){
+        if(sum==0) return 1;
+        return 0;
+      }
+      
+      return fun(n-1, sum+1) + fun(n-1, sum-1);
+    }
+   
+    virtual int findWays(int n){
+      return (n&1)? 0: fun(n, 0);
+    }
+};
