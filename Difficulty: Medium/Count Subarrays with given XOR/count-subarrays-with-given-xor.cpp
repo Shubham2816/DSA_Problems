dcb3481a80887549e528c272919a141c88100820
Shubham Kumar <96763863+Shubham2816@users.noreply.github.com>
2026-02-22 09:58:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        // code here
+          int n=arr.size();
+      vector<int> xorArr(n);
+      xorArr[0]=arr[0];
+      
+      for(int i=1; i<n; i++) xorArr[i]=xorArr[i-1]^arr[i];
+      
+      unordered_map<int, long> visited;
+      long ans=0LL;
+      
+      for(int x: xorArr){
+        if(x==k) ans++;
+        int req=x^k;
+        if(visited.find(req)!=visited.end()) ans+=visited[req];
+        visited[x]++;
+      }
+      return ans;
+    }
+};
